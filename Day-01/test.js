7f603fb4dfd53a4110461f4913cfbe1d9d3c0dc0
Ankit Raj <ankitrajcollab@gmail.com>
2025-05-09 05:17:27 +0530

    (feat): add assignment solution

--- Day-01/test.js
+++ Day-01/test.js
@@ -3,4 +3,6 @@ console.log("here i have written test script")
 
 const docDiv = document.getElementById("someId").innerText = "I am Some Div"
 
- 
+// document.open();
+document.write("<h1>Hello World</h1>");
+// document.close();
