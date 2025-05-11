9112f833bb3a43f894e16828482b0e3101736cc1
Ankit Raj <ankitrajcollab@gmail.com>
2025-05-11 22:54:53 +0530

    add variables code

---
+++ Day-02/index.js
@@ -0,0 +1,39 @@
+console.log("Day-02");
+
+// variables : Varibales are used to store data in js
+
+// - 'var' - Function-scoped, can be redeclared (not recomended)
+// - 'let' - Block-scoped, can be reassigned
+// - 'const' - Block-scoped, **cannot** be reassigned
+
+var address = "Bangalore";
+
+console.log(address);
+
+var address = "india";
+
+console.log(address);
+
+// let can be reassigned but it cant redeclared .
+
+let myName = "Ankit";
+
+console.log(myName);
+
+myName = "rj";
+
+console.log(myName);
+
+// multiple varibale declaration
+
+let name, salary, department;
+
+// this is comments in js - your js program will not treat it as a program
+
+
+// Primitive Data Types:
+
+- String
+- Number 
+- Boolean
+
