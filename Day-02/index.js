9b40f06014d8a4b154c1ad2c0094190e8dc31578
Ankit Raj <ankitrajcollab@gmail.com>
2025-05-12 17:36:25 +0530

    add non add data types code

--- Day-02/index.js
+++ Day-02/index.js
@@ -28,12 +28,49 @@ console.log(myName);
 
 let name, salary, department;
 
+salary = null;
+
 // this is comments in js - your js program will not treat it as a program
 
+// // Primitive Data Types:
 
-// Primitive Data Types:
+// - String - Text values ("Hello")
+// - Number - Numeric values (`25`,`3.14`)
+// - Boolean - True / False
+// - undefined  - a varibale is declared but not assigned
+// - Null - Represents nothing ( let y = null; )
+// - BigInt - Large numbers
+// - Symbol - Unique identifiers
 
-- String
-- Number 
-- Boolean
+// // Non primitive
+
+// - Object - Collection of key-value
+// - Array - Orderd list of values
+// - Function - Code that can be executed
+
+// example of complex data type - using complex data type
+
+let score = 10;
+// Or 
+let student = {
+  name: "Alice",
+  age: 22,
+  isEnrolled: true,
+};
+
+console.log(student.name); // Output: Alice
+
+let arr = [1,2,4,5];
+
+
+// Heap vs Stack 
+
+// How Js See The Code .
+
+// Tokenizing 
+// Parsing - Abstract Syntax Tree . - website - ast explorer.net 
+// Interpreting
+
+
+// 49:05 - Do Assignment.
 
